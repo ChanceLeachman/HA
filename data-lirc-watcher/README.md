f4aec79a3b70276dd71506e4cab856d53d7609e7
Chance Leachman <leachmanchance@gmail.com>
2022-08-27 17:09:21 +0000

    Added watcher files

---
+++ data-lirc-watcher/README.md
@@ -0,0 +1,124 @@
+# LIRC watcher
+![Docker Build](https://github.com/pilotak/docker-lirc-watcher/workflows/docker%20build/badge.svg) ![Docker Pulls](https://img.shields.io/docker/pulls/pilotak/lirc-watcher) ![Docker Size](https://img.shields.io/docker/image-size/pilotak/lirc-watcher?color=orange)
+
+Docker container that listens to LIRC daemon (running on the host) and sends received codes over MQTT with added benefit of short and long putton press. It can also send IR remote keys through lirc by publishing to MQTT topics.
+
+**LIRC must be install on the host system**. Following examples have been tested below but should work on other platforms with adjustments too.
+
+### Debian Buster
+Please follow steps in wiki [Install-on-Debian-Buster](https://github.com/pilotak/docker-lirc-watcher/wiki/Install-on-Debian-Buster)
+
+### Debian Stretch
+Please follow steps in wiki [Install-on-Debian-Stretch](https://github.com/pilotak/docker-lirc-watcher/wiki/Install-on-Debian-Stretch)
+
+### Ubuntu 18.04
+Please follow steps in wiki [Install-on-Ubuntu-18.04](https://github.com/pilotak/docker-lirc-watcher/wiki/Install-on-Ubuntu-18.04)
+
+## Recording codes
+Test receiver
+```sh
+sudo systemctl stop lircd.service
+mode2 --driver default --device /dev/lirc0
+```
+Output should look similar to this:
+```
+space 4195
+pulse 551
+space 1621
+pulse 501
+space 529
+pulse 572
+space 1553
+pulse 547
+space 524
+pulse 551
+space 525
+pulse 549
+```
+
+If everything is ok, execute following and follow the commands in the script. Adjust name of the file should you wish.
+```sh
+sudo irrecord --driver default --device /dev/lirc0 ~/pioneer.lircd.conf
+```
+
+You will end it up with file `~/pioneer.lircd.conf`.
+
+```
+begin remote
+
+  name  pioneer
+  bits           32
+  flags SPACE_ENC|CONST_LENGTH
+  eps            30
+  aeps          100
+
+  header       8544  4180
+  one           578  1545
+  zero          578   497
+  ptrail        575
+  gap          91166
+  min_repeat      3
+  toggle_bit_mask 0xF0F08080
+  frequency    38000
+
+      begin codes
+          KEY_POWER                0xA55A38C7
+          KEY_MUTE                 0xA55A48B7
+          KEY_VOLUMEUP             0xA55A50AF
+          KEY_VOLUMEDOWN           0xA55AD02F
+      end codes
+
+end remote
+```
+
+Let's move this config over to LIRC daemon.
+```sh
+sudo mv ~/pioneer.lircd.conf /etc/lirc/lircd.conf.d/
+sudo systemctl start lircd.service
+```
+
+Test receiver again, if you see the names of your keys when button pressed, that's a win-win.
+```sh
+irw
+```
+
+## Docker-compose
+Now just start the docker container, alter the config to your needs and you ready to rock.
+```yaml
+version: "3"
+services:
+  lirc:
+    container_name: lirc
+    restart: always
+    image: pilotak/lirc-watcher
+    environment:
+      - MQTT_BROKER=192.168.0.10
+      - MQTT_USER=admin
+      - MQTT_PASSWORD=my-secret-pw
+    volumes:
+      - /var/run/lirc/lircd:/var/run/lirc/lircd
+```
+
+### Environmental variables
+Bellow are all available variables
+
+| Variable | Description | Default value |
+| --- | --- | :---:|
+| `LONG_PRESS` | How many messages is received to be considered as long press | 12 |
+| `READ_TIMEOUT` | How long to wait to process new data *seconds* | 0.2 | 
+| `PAYLOAD_LONG_PRESS` | Payload on long press | "long" | 
+| `PAYLOAD_SHORT_CLICK` | Payload on short press | "short" | 
+| `MQTT_BROKER` | Broker address | localhost | 
+| `MQTT_USER` | MQTT user | None | 
+| `MQTT_PASSWORD` | MQTT password | None | 
+| `MQTT_PORT` | MQTT broker port | 1883 | 
+| `MQTT_ID` | MQTT client id | "lirc-watcher" | 
+| `MQTT_PREFIX` | MQTT topic prefix | "lirc" |
+| `MQTT_QOS` | MQTT QOS | 1 |
+
+### MQTT topics for receiving
+When button is pressed you will receive message in format
+`MQTT_PREFIX/REMOTE_NAME/KEY_NAME` with payload `short` / `long` ie. `lirc/pioneer/KEY_POWER`
+
+### MQTT topics for sending
+To send a remote key press, publish to `MQTT_PREFIX/send/REMOTE_NAME/KEY_NAME` with the payload being the number of repeats ie. `lirc/send/pioneer/KEY_POWER` If the payload is empty, repeats will default to 1.
---
+++ data-lirc-watcher/lirc_watcher.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python3
+
+import os
+import paho.mqtt.client as paho
+from threading import Timer
+import sys
+import socket
+import fcntl
+import errno
+from time import sleep
+
+LONG_PRESS = os.getenv('LONG_PRESS', 12)
+READ_TIMEOUT = os.getenv('READ_TIMEOUT', 0.2)
+PAYLOAD_LONG_PRESS = os.getenv('PAYLOAD_LONG_PRESS', 'long')
+PAYLOAD_SHORT_CLICK = os.getenv('PAYLOAD_SHORT_CLICK', 'short')
+MQTT_BROKER = os.getenv('MQTT_BROKER', 'localhost')
+MQTT_USER = os.getenv('MQTT_USER', None)
+MQTT_PASSWORD = os.getenv('MQTT_PASSWORD', None)
+MQTT_QOS = os.getenv('MQTT_QOS', 1)
+MQTT_PORT = os.getenv('MQTT_PORT', 1883)
+MQTT_ID = os.getenv('MQTT_ID', 'lirc-watcher')
+MQTT_PREFIX = os.getenv('MQTT_PREFIX', 'lirc')
+
+MQTT_SEND_TOPIC = '%s/send/#' % MQTT_PREFIX
+MQTT_STATUS_TOPIC = '%s/alive' % MQTT_PREFIX
+MQTT_PAYLOAD_ONLINE = '1'
+MQTT_PAYLOAD_OFFLINE = '0'
+
+print("LIRC watcher started")
+
+
+def on_mqtt_connect(mqtt, userdata, flags, rc):
+    if rc == 0:
+        print('MQTT connected')
+
+        mqtt.publish(MQTT_STATUS_TOPIC, payload=MQTT_PAYLOAD_ONLINE,
+                     qos=MQTT_QOS, retain=True)
+        mqtt.subscribe(MQTT_SEND_TOPIC)
+    else:
+        print('MQTT connect failed:', rc)
+
+def on_mqtt_message(client, userdata, msg):
+    try:
+        (remote, key) = str(msg.topic).split("/")[-2:]
+        try:
+            repeat = int(msg.payload)
+        except:
+            repeat = 1
+        command = "SEND_ONCE %s %s %d\n" % (remote, key, repeat)
+        print(command)
+        sock.sendall(command.encode("utf-8"))
+    except:
+        print(str(msg.topic))
+
+prev_data = None
+timer = None
+
+mqtt = paho.Client(MQTT_ID)
+mqtt.on_connect = on_mqtt_connect
+mqtt.on_message = on_mqtt_message
+mqtt.will_set(MQTT_STATUS_TOPIC, payload=MQTT_PAYLOAD_OFFLINE,
+              qos=MQTT_QOS, retain=True)
+mqtt.username_pw_set(MQTT_USER, MQTT_PASSWORD)
+mqtt.connect(MQTT_BROKER, MQTT_PORT)
+mqtt.loop_start()
+
+sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+sock.connect("/var/run/lirc/lircd")
+fcntl.fcntl(sock, fcntl.F_SETFL, os.O_NONBLOCK)
+
+
+def send_code(priority_data=None):
+    global prev_data, mqtt, timer
+
+    if timer is not None and timer.is_alive():
+        timer.cancel()
+        timer = None
+
+    if priority_data is not None or prev_data is not None:
+        to_send = priority_data if priority_data is not None else prev_data
+        to_send = to_send.split()
+        key_name = to_send[2]
+        remote = to_send[3]
+        counter = int(to_send[1], 16)
+
+        if(counter >= LONG_PRESS):
+            payload = PAYLOAD_LONG_PRESS
+        else:
+            payload = PAYLOAD_SHORT_CLICK
+
+        topic = "%s/%s/%s" % (MQTT_PREFIX, remote, key_name)
+        print("Sending message: '%s' to topic: '%s'" %
+              (payload, topic))
+        mqtt.publish(topic, payload=payload, qos=MQTT_QOS)
+
+        if priority_data is None:
+            prev_data = None
+
+
+try:
+    while True:
+        """
+        Check for new data
+        """
+        try:
+            new_data = sock.recv(128)
+            new_data = new_data.strip()
+
+        except socket.error as e:
+            err = e.args[0]
+
+            """
+            Check for "real" error
+            """
+            if err == errno.EAGAIN or err == errno.EWOULDBLOCK:
+                sleep(0.01)
+                continue
+            else:
+                print(e)
+                mqtt.disconnect()
+                mqtt.loop_stop()
+                sys.exit(1)
+        else:
+            if new_data:
+                new_data = new_data.decode("utf-8")
+                print("new_data: ", new_data)
+                counter_str = new_data.split()
+
+                #ignore the status return from lirc sends
+                if counter_str[0] != "BEGIN":
+                    """
+                    If we received new_data and prev_data was not sent
+                    """
+                    if prev_data is not None and int(counter_str[1], 16) == 0:
+                        send_code(prev_data)
+
+                    prev_data = new_data
+
+                    if timer is not None and timer.is_alive():
+                        timer.cancel()
+
+                    timer = Timer(READ_TIMEOUT, send_code)
+                    timer.start()
+
+
+except KeyboardInterrupt:
+    mqtt.disconnect()
+    mqtt.loop_stop()
